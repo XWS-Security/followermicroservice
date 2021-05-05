e71b2608f96570edfbb6dc801dda5e13e25ca4e3
Milica Siriski <milica.siriski998@gmail.com>
2021-05-05 16:57:43 +0200

    Project init

---
+++ src/main/java/org/nistagram/followermicroservice/FollowermicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.nistagram.followermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowermicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowermicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nistagram/followermicroservice/FollowermicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nistagram.followermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowermicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
