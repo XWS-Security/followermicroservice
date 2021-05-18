36aaa385d6a36df5dde6fd551f3df54d3fc7256a
Milica Siriski <milica.siriski998@gmail.com>
2021-05-18 15:26:13 +0200

    Added an example user and interaction creation.

--- src/main/java/org/nistagram/followermicroservice/controller/UserController.java
+++ src/main/java/org/nistagram/followermicroservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.nistagram.followermicroservice.controller;
 
+import org.nistagram.followermicroservice.data.model.FollowingStatus;
+import org.nistagram.followermicroservice.data.model.Interaction;
 import org.nistagram.followermicroservice.data.model.User;
 import org.nistagram.followermicroservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +25,35 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<String> createAuthenticationToken() {
-        User user = new User() {{
-            setUsername("milica");
-            setProfilePrivate(true);
-            setFollowers(new HashMap<>());
-            setFollowing(new HashMap<>());
-        }};
+        User user = new User();
+        user.setUsername("lule");
+        user.setProfilePrivate(false);
+
+        User user2 = new User();
+        user2.setUsername("dovla");
+        user2.setProfilePrivate(true);
+
+        Interaction interaction = new Interaction();
+        interaction.setFollowingStatus(FollowingStatus.FOLLOWING);
+        interaction.setMuted(false);
+        interaction.setNotificationsOn(true);
+
+        HashMap<User, Interaction> followers = new HashMap<>();
+        followers.put(user2, interaction);
+        user.setFollowers(followers);
+
+        Interaction interaction2 = new Interaction();
+        interaction2.setFollowingStatus(FollowingStatus.WAITING_FOR_APPROVAL);
+        interaction2.setMuted(false);
+        interaction2.setNotificationsOn(false);
+
+        HashMap<User, Interaction> followers2 = new HashMap<>();
+        followers2.put(user, interaction2);
+        user2.setFollowers(followers2);
+
         userService.saveUser(user);
+        userService.saveUser(user2);
+
         return new ResponseEntity<>("User saved", HttpStatus.OK);
     }
 }
