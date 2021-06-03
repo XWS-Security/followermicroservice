86492e4277bae8e05db049141e9f9a26e0ebd303
Milica Siriski <milica.siriski998@gmail.com>
2021-06-03 12:58:17 +0200

    [#7] User can reject follow requests

--- src/main/java/org/nistagram/followermicroservice/controller/InteractionController.java
+++ src/main/java/org/nistagram/followermicroservice/controller/InteractionController.java
@@ -10,10 +10,7 @@ import org.nistagram.followermicroservice.service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -49,11 +46,23 @@ public class InteractionController {
         }
     }
 
-    @PostMapping("/acceptRequest")
+    @PutMapping("/acceptRequest")
     public ResponseEntity<String> acceptFollowRequest(@RequestBody @Valid FollowRequestDto dto) {
         try {
             // TODO: log
             followService.acceptFollowRequest(dto.getFollowerUsername(), dto.getFolloweeUsername());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            loggerService.logException(e.getMessage());
+            return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/rejectRequest")
+    public ResponseEntity<String> rejectFollowRequest(@RequestBody @Valid FollowRequestDto dto) {
+        try {
+            // TODO: log
+            followService.rejectFollowRequest(dto.getFollowerUsername(), dto.getFolloweeUsername());
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             loggerService.logException(e.getMessage());
