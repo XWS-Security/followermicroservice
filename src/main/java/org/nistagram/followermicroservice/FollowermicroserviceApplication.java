2c8dc135b0c484adab1498711e31c7a0a78f572e
Milica Siriski <milica.siriski998@gmail.com>
2021-06-07 18:11:18 +0200

    Added test data, deleted confusing controller method

--- src/main/java/org/nistagram/followermicroservice/FollowermicroserviceApplication.java
+++ src/main/java/org/nistagram/followermicroservice/FollowermicroserviceApplication.java
@@ -1,7 +1,11 @@
 package org.nistagram.followermicroservice;
 
+import org.nistagram.followermicroservice.data.model.Interaction;
 import org.nistagram.followermicroservice.data.model.Role;
+import org.nistagram.followermicroservice.data.model.User;
+import org.nistagram.followermicroservice.data.repository.InteractionRepository;
 import org.nistagram.followermicroservice.data.repository.RoleRepository;
+import org.nistagram.followermicroservice.data.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +26,41 @@ public class FollowermicroserviceApplication {
         Role role = new Role("NISTAGRAM_USER_ROLE");
         List<Role> roles = new ArrayList<>();
         roles.add(role);
-//        roleRepository.save(role);
+        roleRepository.save(role);
+
+        // insert test data
+        User luka = new User("luka", false);
+        User vlado = new User("vlado", false);
+        User vidoje = new User("vidoje", false);
+        User milica = new User("milica", false);
+        User duja = new User("duja", false);
+        User kobra = new User("kobra", false);
+
+        luka.setRoles(roles);
+        vlado.setRoles(roles);
+        vidoje.setRoles(roles);
+        milica.setRoles(roles);
+        duja.setRoles(roles);
+        kobra.setRoles(roles);
+
+        List<User> users = new ArrayList<>();
+        users.add(luka);
+        users.add(vlado);
+        users.add(vidoje);
+        users.add(milica);
+        users.add(duja);
+        users.add(kobra);
+
+        UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
+        userRepository.saveAll(users);
+
+        InteractionRepository interactionRepository = (InteractionRepository) ctx.getBean("interactionRepository");
+        interactionRepository.saveRelationship("luka", "vlado", "FOLLOWING");
+        interactionRepository.saveRelationship("vlado", "luka", "FOLLOWING");
+        interactionRepository.saveRelationship("vlado", "vidoje", "FOLLOWING");
+        interactionRepository.saveRelationship("milica", "duja", "FOLLOWING");
+        interactionRepository.saveRelationship("duja", "milica", "FOLLOWING");
+        interactionRepository.saveRelationship("milica", "luka", "FOLLOWING");
+        interactionRepository.saveRelationship("vlado", "kobra", "FOLLOWING");
     }
 }
